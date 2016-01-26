80b410fc17c56ec25d528b088f544b997b0997da
Eric Kachel <ekachel@redcapemedia.com>
2016-01-26 14:18:46 -0500

    site in progress
    first level of marketing site

---
+++ design-teams.php
@@ -0,0 +1,60 @@
+<!doctype html>
+
+<html lang="en">
+	<head>
+  		<meta charset="utf-8">
+			<title>Red Cape Media, LLC - Communication Design</title>
+  				<meta name="description" content="The HTML5 Herald">
+  					<meta name="Red Cape Media, LLC" content="SitePoint">
+						<link rel="stylesheet" type="text/css" href="redcapemedia-v2.css">
+            <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+	</head>
+		<body>
+        <!--top row-->
+        <div id="colorstrip">
+          <div class="masthead">
+            <div class="masthead-logo">
+              <img src="grfx/red-cape-media-logo.jpg" width="200" height="200">
+            </div>
+            <div class="masthead-nav">
+                
+                <a href="agencies.php">Agencies</a> 
+                <img src="grfx/img-nav-seperator.png" />
+                <a href="design-teams.php">Design Teams</a>
+                <img src="grfx/img-nav-seperator.png" />
+                <a href="marketers.php">Marketers</a>
+                <p>Support for:</p>
+                
+            </div>
+          </div>
+        </div> 
+        <!--middle row-->
+        <div id="content">
+          <div class="middle">
+            <h1>Helping your organization stay lean <br>and operate at maximum efficiency.</h1>
+            <p>Red Cape Media&acute;s strategic design expertise helps organizations:</p>
+
+            <ul>
+            <li>Identify and leverage the 'best of' existing brand assets while working to evolve their visual design language</li>
+            <li>Create processes that allow design teams to work more efficiently and with minimal resources</li>
+            <li>Ensure brand elements are implemented consistently across all touch points of communication</li>
+          </ul>
+
+          <p>We understand that the dynamics and needs of each organization are unique. Red Cape Media's goal is to present solutions that keep your organization lean, maximize your organization's budget while bringing about positive change to people, process and product. </p>
+
+          <p><i>Please contact us <b>(info AT red cape media dot com)</b> for additional information. Check back shortly as our site is currently in development.</i></p>
+            </div>   
+        </div>
+        <div id="footer"> 
+          <div class="footer-content">           
+           &copy;2014-2016. Red Cape Media, LLC. All rights reserved. <a href="privacy.php">Privacy Policy</a>. 
+          </div> 
+        </div>   
+  		<!--bottom row (end) -->
+      
+    </body>
+</html>
---
+++ marketers.php
@@ -0,0 +1,87 @@
+<!doctype html>
+
+<html lang="en">
+	<head>
+  		<meta charset="utf-8">
+			<title>Red Cape Media, LLC - Communication &amp; Design Expertise | Boston, Massachusetts</title>
+  				<meta name="description" content="The HTML5 Herald">
+  					<meta name="Red Cape Media, LLC" content="SitePoint">
+						<link rel="stylesheet" type="text/css" href="redcapemedia-v2.css">
+            <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+	</head>
+		<body>
+        <!--top row-->
+        <div id="colorstrip-inner">
+          <div class="masthead">
+            <div class="masthead-logo-inner">
+              <a href="index.html"><img src="grfx/red-cape-media-logo.jpg" border="0"></a>
+            </div>
+            <div class="masthead-nav-inner">
+                
+                <a href="agencies.php">Agencies</a> 
+                <img src="grfx/img-nav-seperator.png" />
+                <a href="design-teams.php">Design Teams</a>
+                <img src="grfx/img-nav-seperator.png" />
+                <a href="marketers.php">Marketers</a>
+                <p>Support for:</p>
+                
+            </div>
+          </div>
+        </div> 
+        <!--middle row-->
+        <div id="content">
+          <div class="middle">
+            <div class="middle-main">
+                <h2>Dedicated online marketing expertise<br> to promote your organization and strengthen your customer base.</h2>
+                
+                <p>Online marketers usually do the work of a multi&ndash;disciplinary team. Copywriting, email distribution, 
+                coding landing pages, social media posts, web analytics&mdash; a lot goes into implementing
+                a marketing program. </p>
+
+                <p>When organizations forgo a full&ndash;time marketer, the responsibilities of this critical position fall on the employee with the most web know&ndash;how. 
+                This individual may dabble in HTML or tweet on social media, but they lack the desire or expertise to generate tangible results.
+                Marketing efforts appear disjointed, infrequent and ultimately fail to make the right impression.</p>
+
+                <p><b>If you want the best results that <i>don't &quot;break the bank&quot;</i>, partner with Red Cape Media to:</b></p>
+                <ul>
+                  <li>Create the marketing tools needed to communicate online</li>
+                  <li>Promote your events, offers and successes regularly</li>
+                  <li>Monitor and report  on customer interaction(s)</li>
+                  <li>Refine efforts to improve how you reach your audience(s) </li>
+                  <li>Maximize your budget and ensure efficiency</li>
+                </ul>
+
+                <p>Red Cape Media is a virtual marketing arm for organizations that want to regularly communicate online
+                without the overhead of full&ndash;time marketing professionals. We work monthly and quarterly with clients to:</p>
+
+                <ul>
+                  <li>Develop and manage an editorial calendar to promote your organization</li>
+                  <li>Produce content for web sites, e-newsletters and social media</li>
+                  <li>Monitor, analyze and report on how your audience responds to this content</li>
+                  <li>Provide recommendations backed by real data so you can improve your efforts</li>
+                </ul> 
+
+                <p>Let us provide your organization the expertise it needs and deserves to successfully communicate online. Schedule your FREE,  
+                30-minute consultation today.</p> 
+
+            </div>
+                 
+            <div class="middle-rail">
+              <h3>Case Studies</h3>
+            </div>
+            
+       </div>   
+      </div>
+        <div id="footer"> 
+          <div class="footer-content">           
+           &copy;2014-2016. Red Cape Media, LLC. All rights reserved. <a href="privacy.php">Privacy Policy</a>. 
+          </div> 
+        </div>   
+  		<!--bottom row (end) -->
+      
+    </body>
+</html>
